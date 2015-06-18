8822c9e9e8537441d5dd37f695be0478a3b62ce7
Aleksandr Priymak <aleksandr.priymak@gmail.com>
2015-06-18 18:26:05 +0300

    Implement start line parsing

--- core.py
+++ core.py
@@ -1,7 +1,66 @@
+import collections
+import re
+
+RequestLine = collections.namedtuple("RequestLine", ["method", "target", "version"])
+StatusLine = collections.namedtuple("StatusLine", ["version", "code", "reason"])
+
+class Method(object):
+  def __init__(self, value):
+    self._value = value.upper()
+
+  def __cmp__(self, other):
+    if not isinstance(other, Method):
+      other = Method(other)
+    if self._value == other._value:
+      return 0
+    elif self._value < other._value:
+      return -1
+    else:
+      return 1
+
+  def __repr__(self):
+    return repr(self._value)
+
+
+class HttpVersion(object):
+  def __init__(self, major, minor):
+    self.major = major
+    self.minor = minor
+
+  def __eq__(self, other):
+    if isinstance(other, tuple):
+      return (self.major, self.minor) == other
+
+  def __repr__(self):
+    return "HTTP/%d.%d" % (self.major, self.minor)
+
+
+def parse_version(version):
+  match = re.match("^HTTP/(\d).(\d)$", version)
+  return HttpVersion(int(match.group(1)), int(match.group(2)))
+
+
+class BadRequestLineError(ValueError):
+  pass
+
+class BadStatusLineError(ValueError):
+  pass
 
 
 def parse_request_line(line):
-  pass
+  parts = line.split(" ")
+  if len(parts) != 3:
+    raise BadRequestLineError("Unable to parse request line")
+
+  return RequestLine(Method(parts[0]), parts[1], parse_version(parts[2]));
+
 
 def parse_status_line(line):
-  pass
+  parts = line.split(" ", 2)
+  if len(parts) != 3:
+    raise BadStatusLineError("Unable to parse status line")
+
+  try:
+    return StatusLine(parse_version(parts[0]), int(parts[1]), parts[2])
+  except ValueError:
+    raise BadStatusLineError("Unable to parse status line: bad status code")
--- test_core.py
+++ test_core.py
@@ -6,6 +6,22 @@ import pytest
 def test_nothing():
 	pass
 
+def test_method_compare():
+  assert core.Method("get") == "GET"
+  assert core.Method("GET") == core.Method("get")
+
+def test_parse_version():
+  version = core.parse_version("HTTP/1.1")
+  assert version == (1, 1)
+
+  version = core.parse_version("HTTP/1.0")
+  assert version == (1, 0)
+
+def test_parse_bad_version():
+  for bad in ["HTTP/1", "FTP/1.0", "HTTP/a.0", "HTTP/12.3", " HTTP/1.1 "]:
+    with pytest.raises(Exception):
+      core.parse_version(bad)
+
 def test_parse_request_line():
   rline = core.parse_request_line("GET /wiki/article?q=35 HTTP/1.1")
   assert rline.method == "GET"
@@ -13,5 +29,19 @@ def test_parse_request_line():
   assert rline.version == (1, 1)
 
 def test_parse_space_in_target_request_line():
-  with pytest.raises():
+  with pytest.raises(core.BadRequestLineError):
     core.parse_request_line("GET /wiki/russian federation HTTP/1.1")
+
+def test_parse_status_line():
+  sline = core.parse_status_line("HTTP/1.1 200 OK")
+  assert sline.version == (1, 1)
+  assert sline.code == 200
+  assert sline.reason == "OK"
+
+def test_bad_status_line():
+  for bad in [
+    "HTTP/1.0 35x Some reason",
+    "HTTP/1.1 200"
+  ]:
+    with pytest.raises(core.BadStatusLineError):
+      core.parse_status_line(bad)
